bf654ca31426fdc8e34ba21a395775f1ee107ff1
K-Musty <kmustapha9564@gmail.com>
2023-12-09 14:51:23 -0800

    This commit would change just a little

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <wchar.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+typedef struct cool_command_
+{
+	char **command;
+	struct cool_command_ *next;
+} cool_command;
+
+
+#endif
